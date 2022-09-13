f429f963a955850134ac9c0e000f590b889a7738
Alex Cleveland <Alexcleveland5@gmail.com>
2022-09-12 18:05:25 -0600

    moved database to its own file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //Require express and mysql
 const express = require('express');
-const mysql = require('mysql2');
-
 
 //Create Port variable and instance of express
 const PORT = process.env.PORT || 3001;
@@ -10,18 +8,6 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-);
-
 
 
 app.listen(PORT, () => {
