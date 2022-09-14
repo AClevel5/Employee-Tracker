295b93627da0980afe6fbf5c8e3d094a92300de6
Alex Cleveland <Alexcleveland5@gmail.com>
2022-09-14 16:56:29 -0600

    get departments broken, switched starting point to inquire prompts

--- db/database.js
+++ db/database.js
@@ -10,4 +10,4 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
-module.exports = { db };
+module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Require express and mysql
 const express = require('express');
+const { startApp } = require('./lib/index');
 
 //Create Port variable and instance of express
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+startApp();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
